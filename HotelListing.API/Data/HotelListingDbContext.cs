f78c9cfcfceaf3a04e953cbbd33a053ffe0394a7
Tauriq Hendricks <tauriqh786@gmail.com>
2022-04-21 11:10:15 +0200

    Implement Data Classes and Perform Migrations

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -1,7 +1,12 @@
-﻿namespace HotelListing.API.Data
+﻿using HotelListing.API.Data.Model;
+
+namespace HotelListing.API.Data
 {
     public class HotelListingDbContext : DbContext
     {
         public HotelListingDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
     }
 }
---
+++ HotelListing.API/Data/Model/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelListing.API.Data.Model
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+
+        public virtual IList<Hotel> Hotels { get; set; }
+    }
+}
