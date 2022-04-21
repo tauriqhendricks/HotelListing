b437251fd3fd74592e108fdeee818490d0ac8d69
Tauriq Hendricks <tauriqh786@gmail.com>
2022-04-21 11:19:37 +0200

    Seed Data in Tables

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -4,9 +4,62 @@ namespace HotelListing.API.Data
 {
     public class HotelListingDbContext : DbContext
     {
-        public HotelListingDbContext(DbContextOptions options): base(options) { }
+        public HotelListingDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    Code = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    Code = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Islands",
+                    Code = "CI"
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+            );
+        }
     }
 }
